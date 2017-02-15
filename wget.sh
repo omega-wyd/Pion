f555d4c83520b14f6bde76a85d00b3ef878c582d
David Mariluch <davidmariluch@mail.weber.edu>
2017-02-15 04:55:21 +0000

    Added templates for scripts

---
+++ wget.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: wget.sh
+# 
+#         USAGE: ./wget.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: 
+#  ORGANIZATION: WSU
+#       CREATED: 02/15/2017 04:54
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+exit 0
+
