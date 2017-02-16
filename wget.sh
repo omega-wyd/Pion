2e71c56cdabb9e841993a4166bcd7d32b624ed20
Michael Brewer <mbrewerramirez@mail.weber.edu>
2017-02-16 18:17:10 +0000

    wget and wrapper modified

--- wget.sh
+++ wget.sh
@@ -5,19 +5,40 @@
 # 
 #         USAGE: ./wget.sh 
 # 
-#   DESCRIPTION: 
-# 
+#   DESCRIPTION: This script is meant to retrive two files based on the
+# 					input parameter.
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: 
-#  ORGANIZATION: WSU
+#        AUTHOR: Micheal Brewer II Email: mbrewerramirez@mail.weber.edu
+#  ORGANIZATION: Pion
 #       CREATED: 02/15/2017 04:54
 #      REVISION:  ---
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
+
+
+#help function with script usage
+
+usage()
+{
+	echo "Usage: $0 [year1] [year2]"
+}
+
+# Check for help call
+if [[ $1 == "--help" ]]
+then 
+	usage
+	exit 1
+fi
+
+
+year1=$1
+address="www.icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$year.tar.gz"
+
+wget $address 
 
 
 
--- wrapper.sh
+++ wrapper.sh
@@ -5,19 +5,76 @@
 # 
 #         USAGE: ./wrapper.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: This script controls 6 other scripts written by team Pion. 
 # 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: 
-#  ORGANIZATION: WSU
-#       CREATED: 02/15/2017 04:54
+#        AUTHOR: Micheal Brewer (), mbrewerramirez@mail.weber.edu
+#  ORGANIZATION: Pion
+#       CREATED: 02/15/2017 20:19
 #      REVISION:  ---
 #===============================================================================
 
-#set -o nounset                              # Treat unset variables as an error
+#set -o nounset                  # Treat unset variables as an error
+
+
+# how the script is suppose to be used.
+usage()
+{
+	echo "Invalid Option"
+	echo "Usage: $0 [-y year1,year2] [-e email] [-u username] [-p passwd]"
+	echo " -y -e are required."
+	echo "Enter -u and -p for ftp access."
+	exit 1
+}
+
+# if the user desires to use $0 --help
+if [[ $1 == "--help" ]]
+then
+	usage
+fi
+
+
+# Getopts while loop
+while getopts ":y:e:u:p:" opt
+do 
+	case $opt in
+		y) year=$OPTARG # year to find the appropriate file.
+			;;
+		e) email=$OPTARG #email to send mail notifications
+			;;
+		u) user=$OPTARG #username for ftp account	
+			;;
+		p) passwd=$OPTARG # password needed for ftp server.
+			;;
+		\?)
+			usage
+			;;
+	esac
+done
+
+
+#IF statement to test if parameters are set. -z checks to see if value is null
+if [[ -z $year ]] || [[ -z $email ]]
+then 
+	usage
+fi
+
+
+# divide the substring of year and save each year to a variable.
+
+IFS=, read year1 year2 <<< $year
+
+#check to see if year is 2015 or 2016
+if [[ $year1 != 2015 ]] 
+then
+	echo "needs to be 2015 or 2016"
+fi
+
+
+
 
 
 
