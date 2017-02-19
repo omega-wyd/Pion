82180f17dc9ecf75a6018ea005f907a4994cfba5
Mashael2 <ma01412@icarus.cs.weber.edu>
2017-02-19 19:55:38 +0000

    Update

--- compress.sh
+++ compress.sh
@@ -5,13 +5,13 @@
 # 
 #         USAGE: ./compress.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: This script will compress the final filter file using zip.
 # 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: 
+#        AUTHOR: Mashael Al Abbad (), malabbad1@mail.weber.edu
 #  ORGANIZATION: WSU
 #       CREATED: 02/15/2017 04:53
 #      REVISION:  ---
@@ -19,7 +19,28 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+# Help function
+help ()
+{
+	echo "Usage ./compress.sh"
+	echo "compress the final filter file using zip"
+}
 
+# Check for help call
+if [[ $1 == "--help" ]]
+then
+	help
+	exit 1
+fi
+
+# Compress the final filter file using zip 
+# Rename it with MOCK_DATA_FILTER_YYYY_MM_DD_HH:MM.zip
+date=`date +%Y_%m_%d_%H:%M`
+theFile=MOCK_DATA_FILTER_$date.zip
+
+echo "Zipping up new file"
+
+zip $theFile filename.csv
 
 exit 0
 
--- delete.sh
+++ delete.sh
@@ -5,13 +5,13 @@
 # 
 #         USAGE: ./delete.sh 
 # 
-#   DESCRIPTION: 
+#   DESCRIPTION: Delete the temp folder with all the files.
 # 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: 
+#        AUTHOR: Mashael Al Abbad (), malabbad1@mail.weber.edu
 #  ORGANIZATION: WSU
 #       CREATED: 02/14/2017 20:27
 #      REVISION:  ---
@@ -19,6 +19,9 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+echo "Cleaning up.."
+
+rm -r $PWD/tmp
 
 
 exit 0
