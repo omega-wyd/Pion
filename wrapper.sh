95df8dae6b60868ecee1f3cf6d5da33c961360f1
Michael Brewer <mbrewerramirez@mail.weber.edu>
2017-02-19 23:45:57 +0000

    added all script calls need to test when al scripts are done

--- wrapper.sh
+++ wrapper.sh
@@ -83,8 +83,13 @@ fi
 # Calling expand.sh to expand files.
 ./expand.sh |
 # Calling filter.sh 
-./filter.sh
-
+./filter.sh |
+# Calling compress.sh to compress new file
+./compress.sh |
+#calling ftp.sh
+./ftp.sh |
+#calling delete.sh  
+./delete.sh
 
 
 
