f5392415787e054b27de1ca87ce12a2d243d845a
Michael Brewer <mbrewerramirez@mail.weber.edu>
2017-02-21 01:05:56 +0000

    added a rough skeleton

--- ftp.sh
+++ ftp.sh
@@ -20,6 +20,34 @@
 #set -o nounset                              # Treat unset variables as an error
 
 
+user=$1
+pass=$2
+HOST=' @icarus.cs.weber.edu ' #address
+FILE='/$PWD/temp/outfile.csv ' #file to send  DONT know the zip ext
+
+
+
+
+if [[ ! -z $1 ]] && [[ !-z  $2 ]]
+then
+#use pass and username
+
+`ftp -n $HOST << END_SCRIPT
+quote USER $user
+quote PASS $pass
+binary
+put $FILE
+quit
+END_SCRIPT`
+
+else
+
+#use annonymous 
+
+
+
+
+
 
 exit 0
 
